d38d5c8525e46da119d1615c979a81c5d293cdc6
Christopher Minish <chrisminish@gmail.com>
2022-05-16 10:52:37 -0700

    Initial implementation of the Student/Course models. Added annotations and fields along with helper methods and the overrides for the equals and hashcode methods

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,69 @@
 package sba.sms.models;
 
-public class Course {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Table(name = "course")
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @NonNull
+    @Column(length = 50, nullable = false)
+    String name;
+
+    @NonNull
+    @Column(length = 50, nullable = false)
+    String instructor;
+
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST})
+    List<Student> students = new ArrayList<>();
+
+    /*
+        Helper methods for maintaining the list of students and courses in the
+        respective models for the many-to-many relationship
+    */
+
+    public void addStudent(Student student) {
+        students.add(student);
+        student.getCourses().add(this);
+    }
+
+    public void removeStudent(String email) {
+
+    }
+
+    /*
+        Checks if the parameter Object is the same as this Course Object, then
+        if not checks if the parameter Object is a Course and returns a comparison
+        based on the unique ID fields of this Course and parameter Course
+    */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Course course = (Course) o;
+
+        return getId() == course.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,71 @@
 package sba.sms.models;
 
-public class Student {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.Hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Table(name = "student")
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Student {
+    @Id
+    @NonNull
+    @Column(length = 50, nullable = false, name = "email")
+    String email;
+
+    @NonNull
+    @Column(length = 50, nullable = false, name = "name")
+    String name;
+
+    @NonNull
+    @Column(length = 50, nullable = false, name = "password")
+    String password;
+
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST},
+        fetch = FetchType.EAGER)
+    @JoinTable(name = "student_courses",
+        joinColumns = @JoinColumn(name = "student_email"),
+        inverseJoinColumns = @JoinColumn(name = "courses_id"))
+    List<Course> courses = new ArrayList<>();
+
+    public void addCourse(Course course) {
+        courses.add(course);
+        course.getStudents().add(this);
+    }
+
+    public void removeCourse(int courseID) {
+
+    }
+
+    /*
+        Checks to ensure that the object to be compared is a Student
+        and then compares the unique identifier of the Student's email
+        to see if this Student is the same as the parameter Student
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+
+        Student student = (Student) o;
+
+        return Objects.equals(getEmail(), student.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
